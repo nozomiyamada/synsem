eb388c7c1f59aca2e01eb6dd9e28a0de75b8322f
Nozomi Yamada <44984892+nozomiyamada@users.noreply.github.com>
2022-10-11 15:07:18 +0700

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, jsonify
-import os, sys, re, collections, csv, glob, time, itertools
+import os, datetime
+import pandas as pd
 
 ### instantiate app
 class CustomFlask(Flask):
@@ -11,14 +12,37 @@ class CustomFlask(Flask):
 
 app = CustomFlask(__name__)
 
+STIMULI = pd.read_csv('data/stimuli_original.csv') ## id, sentence
+FILLERS = pd.read_csv('data/fillers.csv') ## id, filler
+RESULTS = pd.read_csv('data/result.csv') ## date, name, age, gender, stimulus, is_filler, naturality
+
 ###########################################################
 
 @app.route("/", methods=['GET', 'POST'])
 def top_page():
+    
     ### TOP PAGE ###
     if request.method == 'GET':
-        return render_template('testpage.html')
+        selected_stimuli = STIMULI.sample(5)
+        selected_fillers = FILLERS.sample(5)
+        stimuli = pd.concat([selected_stimuli, selected_fillers]).sample(frac=1)['sentence'].to_list()
+        return render_template('testpage.html', stimuli=stimuli)
+    ### receive ajax json
+    elif request.method == 'POST':
+        ### record result
+        now = str(datetime.datetime.today()).split('.')[0]
+        name = request.form['name']
+        age = request.form['age']
+        gender = request.form['gender']
+        stimuli = request.form.getlist('stimuli[]')
+        answers = request.form.getlist('answers[]')
+        ## RESULTS : date,name,age,gender,stimulus,naturality,is_stimulus
+        for sti, ans in zip(stimuli, answers):
+            is_stimulus = sti in STIMULI.sentence
+            RESULTS.loc[len(RESULTS)] = [now, name, age, gender, sti, ans, is_stimulus]
+        RESULTS.to_csv('data/result2.csv', index=False)
 
+        return jsonify({'status':'success'})
 
 ###########################################################
 
