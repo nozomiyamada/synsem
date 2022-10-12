77306e62e35a805e23f374faecc0d35d71b7ef05
Nozomi Yamada <44984892+nozomiyamada@users.noreply.github.com>
2022-10-12 12:49:40 +0700

    update

--- app.py
+++ app.py
@@ -39,10 +39,11 @@ def top_page():
         gender = request.form['gender']
         stimuli = request.form.getlist('stimuli[]')
         answers = request.form.getlist('answers[]')
+        time_record = request.form.getlist('time_record[]')
         ## RESULTS : date,name,age,gender,stimulus,naturality,is_stimulus
-        for sti, ans in zip(stimuli, answers):
+        for sti, ans, tm in zip(stimuli, answers, time_record):
             is_stimulus = sti in STIMULI.sentence.values
-            RESULTS.loc[len(RESULTS)] = [now, id_, age, gender, sti, ans, is_stimulus]
+            RESULTS.loc[len(RESULTS)] = [now, id_, age, gender, sti, ans, is_stimulus, tm]
         RESULTS.to_csv('static/result.csv', index=False)
         RESULTS.to_json('static/result.json', orient='records')
 
